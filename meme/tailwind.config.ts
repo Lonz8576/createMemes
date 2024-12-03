9c4eba36de82a734d44dbd26f1abe768c7e63681
Nic Alonso <167815213+Lonz8576@users.noreply.github.com>
2024-12-03 17:54:18 -0500

    Updated TS file

--- meme/tailwind.config.ts
+++ meme/tailwind.config.ts
@@ -33,6 +33,95 @@ export default {
       cute: ['var(--cute-font)'],
       xl: ['var(--large-font)'],
     },
+    textSizes: {
+      '4xs': '.375rem',   // 6px
+      '3xs': '.5rem',     // 8px
+      '2xs': '.625rem',   // 10px
+      'xs': '.75rem',     // 12px
+      'sm': '.875rem',    // 14px
+      'base': '1rem',     // 16px
+      'lg': '1.125rem',   // 18px
+      'xl': '1.25rem',    // 20px
+      '2xl': '1.5rem',    // 24px
+      '3xl': '1.875rem',  // 30px
+      '4xl': '2.25rem',   // 36px
+      '5xl': '3rem',      // 48px
+      '6xl': '4rem',      // 64px
+    },
+    tracking: {
+      'condensed': '-.15em',
+      'tightest': '-.1em',
+      'tighter': '-.05em',
+      'tight': '-.025em',
+      'normal': '0',
+      'wide': '.025em',
+      'wider': '.05em',
+      'widest': '.1em',
+      'extended': '.15em',
+    },
+    backgroundSize: {
+      'auto': 'auto',
+      'cover': 'cover',
+      'contain': 'contain',
+    },
+    borderWidths: {
+      default: '1px',
+      '0': '0',
+      '2': '2px',
+      '4': '4px',
+      '8': '8px',
+    },
+    
+  shadows: {
+    default: '0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06)',
+    'md': '0 4px 6px -1px rgba(0, 0, 0, .1), 0 2px 4px -1px rgba(0, 0, 0, .06)',
+    'lg': '0 10px 15px -3px rgba(0, 0, 0, .1), 0 4px 6px -2px rgba(0, 0, 0, .05)',
+    'xl': '0 20px 25px -5px rgba(0, 0, 0, .1), 0 10px 10px -5px rgba(0, 0, 0, .04)',
+    '2xl': '0 25px 50px -12px rgba(0, 0, 0, .25)',
+    'inner': 'inset 0 2px 4px 0 rgba(0,0,0,0.06)',
+    'outline': '0 0 0 3px rgba(52,144,220,0.5)',
+    'none': 'none',
+  },
+  padding: {
+    'px': '1px',
+    '0': '0',
+    '1': '0.25rem',
+    '2': '0.5rem',
+    '3': '0.75rem',
+    '4': '1rem',
+    '5': '1.25rem',
+    '6': '1.5rem',
+    '8': '2rem',
+    '10': '2.5rem',
+    '12': '3rem',
+    '16': '4rem',
+    '20': '5rem',
+    '24': '6rem',
+    '32': '8rem',
+    '40': '10rem',
+  },
+  margin: {
+    'auto': 'auto',
+    'px': '1px',
+    '0': '0',
+    '1': '0.25rem',
+    '2': '0.5rem',
+    '3': '0.75rem',
+    '4': '1rem',
+    '5': '1.25rem',
+    '6': '1.5rem',
+    '8': '2rem',
+    '10': '2.5rem',
+    '12': '3rem',
+    '16': '4rem',
+    '20': '5rem',
+    '24': '6rem',
+    '32': '8rem',
+    '40': '10rem',
+  },
+
+
+  
     extend: {
       spacing: {
         '8xl': '96rem',
