41d57f0d435f3b8b4f5ce10a466b93a1c12ea612
Nic Alonso <167815213+Lonz8576@users.noreply.github.com>
2024-12-03 18:18:22 -0500

    update page.tsx

--- meme/src/app/page.tsx
+++ meme/src/app/page.tsx
@@ -2,8 +2,14 @@
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center h-96 p-8 pb-20 gap-16 sm:p-20 font-display">
+    <div className="grid grid-rows-[20px_1fr_20px] relative items-center justify-items-center h-96 p-8 pb-20 gap-16 sm:p-20 font-display">
      
+     <div className="flex relative justify-center">
+    
+     </div>
+
+
+
     </div>
   );
 }
