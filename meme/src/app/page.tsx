6891263e39d495a596a5ff4962ed5c0201dee2e1
Nic Alonso <167815213+Lonz8576@users.noreply.github.com>
2024-12-07 15:47:13 -0500

    imagekit sdk install

--- meme/src/app/page.tsx
+++ meme/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] relative items-center justify-items-center h-96 p-8 pb-20 gap-16 sm:p-20 font-display">
      
      <div className="flex relative justify-center">
-    <Image src="https://ik.imagekit.io/yllpuzoch/meme%20library%20/tr:w-300,h-300/cloned.jpg?updatedAt=1733602656646" alt="clone meme"></Image>
+    <Image src="https://ik.imagekit.io/yllpuzoch/meme%20library%20/cloned.jpg?updatedAt=1733602656646" alt="clone meme"></Image>
      </div>
 
 
